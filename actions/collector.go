9d76404e409db70325ab5425e978f9e2b2bb3d3f
Julien Levesy <jlevesy@gmail.com>
2023-12-06 15:04:10 +0100

    feat(actions): export last refresh timestamp and last refresh duration as gauges

--- actions/collector.go
+++ actions/collector.go
@@ -10,8 +10,26 @@ import (
 	"go.uber.org/zap"
 )
 
+type UsageCollectorOpt func(c *UsageCollector)
+
+func WithNowFunc(fn func() time.Time) UsageCollectorOpt {
+	return func(c *UsageCollector) {
+		c.nowFunc = fn
+	}
+}
+
+type SinceFunc func(time.Time, time.Time) time.Duration
+
+func WithSinceFunc(fn SinceFunc) UsageCollectorOpt {
+	return func(c *UsageCollector) {
+		c.sinceFunc = fn
+	}
+}
+
 type UsageCollector struct {
-	billableTimeDescription *prometheus.Desc
+	billableTimeDesc        *prometheus.Desc
+	lastRefreshTimeDesc     *prometheus.Desc
+	lastRefreshDurationDesc *prometheus.Desc
 
 	refreshTicker *time.Ticker
 	cancelFunc    func()
@@ -20,13 +38,17 @@ type UsageCollector struct {
 
 	ready chan struct{}
 
-	lastUsageDataMu sync.RWMutex
-	lastUsageData   Usage
+	lastUsageDataMu     sync.RWMutex
+	lastUsageData       Usage
+	lastRefreshTime     time.Time
+	lastRefreshDuration time.Duration
 
-	logger *zap.Logger
+	logger    *zap.Logger
+	nowFunc   func() time.Time
+	sinceFunc SinceFunc
 }
 
-func NewUsageCollector(usagefetcher WorkflowUsageFetcher, logger *zap.Logger, refreshPeriod time.Duration) *UsageCollector {
+func NewUsageCollector(usagefetcher WorkflowUsageFetcher, logger *zap.Logger, refreshPeriod time.Duration, opts ...UsageCollectorOpt) *UsageCollector {
 	ctx, cancel := context.WithCancel(context.Background())
 
 	c := UsageCollector{
@@ -35,13 +57,31 @@ func NewUsageCollector(usagefetcher WorkflowUsageFetcher, logger *zap.Logger, re
 		logger:        logger,
 		ready:         make(chan struct{}),
 		usagefetcher:  usagefetcher,
+		nowFunc:       time.Now,
+		sinceFunc:     since,
 
-		billableTimeDescription: prometheus.NewDesc(
+		billableTimeDesc: prometheus.NewDesc(
 			"github_actions_workflow_billable_time_seconds",
 			"Billable time for a repo, per workflow and platform",
 			[]string{"owner", "repo", "workflow", "workflow_id", "platform"},
 			nil,
 		),
+		lastRefreshTimeDesc: prometheus.NewDesc(
+			"github_actions_workflow_last_refresh_timestamp_seconds",
+			"Last timestamp in seconds since epoch of the last dataset refresh",
+			nil,
+			nil,
+		),
+		lastRefreshDurationDesc: prometheus.NewDesc(
+			"github_actions_workflow_last_refresh_duration_seconds",
+			"Last refresh duration in seconds",
+			nil,
+			nil,
+		),
+	}
+
+	for _, opt := range opts {
+		opt(&c)
 	}
 
 	go func() {
@@ -62,7 +102,9 @@ func NewUsageCollector(usagefetcher WorkflowUsageFetcher, logger *zap.Logger, re
 	return &c
 }
 func (c *UsageCollector) Describe(ch chan<- *prometheus.Desc) {
-	ch <- c.billableTimeDescription
+	ch <- c.billableTimeDesc
+	ch <- c.lastRefreshTimeDesc
+	ch <- c.lastRefreshDurationDesc
 }
 
 func (c *UsageCollector) Collect(ch chan<- prometheus.Metric) {
@@ -72,7 +114,7 @@ func (c *UsageCollector) Collect(ch chan<- prometheus.Metric) {
 	for _, workflowData := range c.lastUsageData {
 		for platform, value := range workflowData.BillableTime {
 			ch <- prometheus.MustNewConstMetric(
-				c.billableTimeDescription,
+				c.billableTimeDesc,
 				prometheus.GaugeValue,
 				value.Seconds(),
 				workflowData.Owner,
@@ -82,6 +124,22 @@ func (c *UsageCollector) Collect(ch chan<- prometheus.Metric) {
 				platform,
 			)
 		}
+	}
+
+	if !c.lastRefreshTime.IsZero() {
+		ch <- prometheus.MustNewConstMetric(
+			c.lastRefreshTimeDesc,
+			prometheus.GaugeValue,
+			float64(c.lastRefreshTime.Unix()),
+		)
+	}
+
+	if c.lastRefreshDuration != 0 {
+		ch <- prometheus.MustNewConstMetric(
+			c.lastRefreshDurationDesc,
+			prometheus.GaugeValue,
+			c.lastRefreshDuration.Seconds(),
+		)
 	}
 }
 
@@ -99,6 +157,7 @@ func (c *UsageCollector) Ready() <-chan struct{} {
 func (c *UsageCollector) refresh(ctx context.Context) {
 	c.logger.Info("Refreshing usage data")
 
+	startTime := c.nowFunc()
 	usageData, err := c.usagefetcher.Fetch(ctx)
 	if err != nil {
 		c.logger.Error(
@@ -108,10 +167,17 @@ func (c *UsageCollector) refresh(ctx context.Context) {
 
 		return
 	}
+	endTime := c.nowFunc()
 
-	c.logger.Info("Done refreshing usage data")
+	duration := c.sinceFunc(startTime, endTime)
+
+	c.logger.Info("Done refreshing usage data", zap.Duration("took", duration))
 
 	c.lastUsageDataMu.Lock()
 	c.lastUsageData = usageData
+	c.lastRefreshDuration = duration
+	c.lastRefreshTime = endTime
 	c.lastUsageDataMu.Unlock()
 }
+
+func since(t1, t2 time.Time) time.Duration { return t2.Sub(t1) }
--- actions/collector_test.go
+++ actions/collector_test.go
@@ -15,22 +15,8 @@ import (
 )
 
 var (
-	billableMetrics = `
-# HELP github_actions_workflow_billable_time_seconds Billable time for a repo, per workflow and platform
-# TYPE github_actions_workflow_billable_time_seconds gauge
-github_actions_workflow_billable_time_seconds{owner="totocorp",platform="UBUNTU",repo="repo-A",workflow="build",workflow_id="1"} 15
-github_actions_workflow_billable_time_seconds{owner="totocorp",platform="UBUNTU",repo="repo-A",workflow="release",workflow_id="3"} 15
-github_actions_workflow_billable_time_seconds{owner="totocorp",platform="UBUNTU",repo="repo-A",workflow="run",workflow_id="4"} 15
-github_actions_workflow_billable_time_seconds{owner="totocorp",platform="UBUNTU",repo="repo-A",workflow="test",workflow_id="2"} 15
-github_actions_workflow_billable_time_seconds{owner="totocorp",platform="UBUNTU",repo="repo-B",workflow="build",workflow_id="1"} 15
-github_actions_workflow_billable_time_seconds{owner="totocorp",platform="UBUNTU",repo="repo-B",workflow="release",workflow_id="3"} 15
-github_actions_workflow_billable_time_seconds{owner="totocorp",platform="UBUNTU",repo="repo-B",workflow="run",workflow_id="4"} 15
-github_actions_workflow_billable_time_seconds{owner="totocorp",platform="UBUNTU",repo="repo-B",workflow="test",workflow_id="2"} 15
-github_actions_workflow_billable_time_seconds{owner="totocorp",platform="UBUNTU",repo="repo-C",workflow="build",workflow_id="1"} 15
-github_actions_workflow_billable_time_seconds{owner="totocorp",platform="UBUNTU",repo="repo-C",workflow="release",workflow_id="3"} 15
-github_actions_workflow_billable_time_seconds{owner="totocorp",platform="UBUNTU",repo="repo-C",workflow="run",workflow_id="4"} 15
-github_actions_workflow_billable_time_seconds{owner="totocorp",platform="UBUNTU",repo="repo-C",workflow="test",workflow_id="2"} 15
-`
+	now = time.Date(2023, 10, 15, 0, 0, 0, 0, time.UTC)
+
 	repos = []any{
 		[]github.Repository{
 			{
@@ -77,6 +63,7 @@ github_actions_workflow_billable_time_seconds{owner="totocorp",platform="UBUNTU"
 			},
 		},
 	}
+
 	workflows = []any{
 		github.Workflows{
 			Workflows: []*github.Workflow{
@@ -103,11 +90,22 @@ github_actions_workflow_billable_time_seconds{owner="totocorp",platform="UBUNTU"
 			},
 		},
 	}
+
 	workflowTiming = github.WorkflowUsage{
 		Billable: &github.WorkflowBillMap{
 			"UBUNTU": &github.WorkflowBill{
 				TotalMS: ptr(int64(15000)),
-			}},
+			},
+		},
+	}
+
+	defaultMockBehavior = []mock.MockBackendOption{
+		mock.WithRequestMatchPages(mock.GetOrgsReposByOrg, repos...),
+		mock.WithRequestMatchPages(mock.GetReposActionsWorkflowsByOwnerByRepo, workflows...),
+		mock.WithRequestMatchPages(
+			mock.GetReposActionsWorkflowsTimingByOwnerByRepoByWorkflowId,
+			workflowTiming,
+		),
 	}
 )
 
@@ -118,16 +116,42 @@ func TestCollector(t *testing.T) {
 		wantMetrics string
 	}{
 		{
-			metricName: "github_actions_workflow_billable_time_seconds",
-			mockOptions: []mock.MockBackendOption{
-				mock.WithRequestMatchPages(mock.GetOrgsReposByOrg, repos...),
-				mock.WithRequestMatchPages(mock.GetReposActionsWorkflowsByOwnerByRepo, workflows...),
-				mock.WithRequestMatchPages(
-					mock.GetReposActionsWorkflowsTimingByOwnerByRepoByWorkflowId,
-					workflowTiming,
-				),
-			},
-			wantMetrics: billableMetrics,
+			metricName:  "github_actions_workflow_billable_time_seconds",
+			mockOptions: defaultMockBehavior,
+			wantMetrics: `
+# HELP github_actions_workflow_billable_time_seconds Billable time for a repo, per workflow and platform
+# TYPE github_actions_workflow_billable_time_seconds gauge
+github_actions_workflow_billable_time_seconds{owner="totocorp",platform="UBUNTU",repo="repo-A",workflow="build",workflow_id="1"} 15
+github_actions_workflow_billable_time_seconds{owner="totocorp",platform="UBUNTU",repo="repo-A",workflow="release",workflow_id="3"} 15
+github_actions_workflow_billable_time_seconds{owner="totocorp",platform="UBUNTU",repo="repo-A",workflow="run",workflow_id="4"} 15
+github_actions_workflow_billable_time_seconds{owner="totocorp",platform="UBUNTU",repo="repo-A",workflow="test",workflow_id="2"} 15
+github_actions_workflow_billable_time_seconds{owner="totocorp",platform="UBUNTU",repo="repo-B",workflow="build",workflow_id="1"} 15
+github_actions_workflow_billable_time_seconds{owner="totocorp",platform="UBUNTU",repo="repo-B",workflow="release",workflow_id="3"} 15
+github_actions_workflow_billable_time_seconds{owner="totocorp",platform="UBUNTU",repo="repo-B",workflow="run",workflow_id="4"} 15
+github_actions_workflow_billable_time_seconds{owner="totocorp",platform="UBUNTU",repo="repo-B",workflow="test",workflow_id="2"} 15
+github_actions_workflow_billable_time_seconds{owner="totocorp",platform="UBUNTU",repo="repo-C",workflow="build",workflow_id="1"} 15
+github_actions_workflow_billable_time_seconds{owner="totocorp",platform="UBUNTU",repo="repo-C",workflow="release",workflow_id="3"} 15
+github_actions_workflow_billable_time_seconds{owner="totocorp",platform="UBUNTU",repo="repo-C",workflow="run",workflow_id="4"} 15
+github_actions_workflow_billable_time_seconds{owner="totocorp",platform="UBUNTU",repo="repo-C",workflow="test",workflow_id="2"} 15
+`,
+		},
+		{
+			metricName:  "github_actions_workflow_last_refresh_duration_seconds",
+			mockOptions: defaultMockBehavior,
+			wantMetrics: `
+# HELP github_actions_workflow_last_refresh_duration_seconds Last refresh duration in seconds
+# TYPE github_actions_workflow_last_refresh_duration_seconds gauge
+github_actions_workflow_last_refresh_duration_seconds 1
+				`,
+		},
+		{
+			metricName:  "github_actions_workflow_last_refresh_timestamp_seconds",
+			mockOptions: defaultMockBehavior,
+			wantMetrics: `
+# HELP github_actions_workflow_last_refresh_timestamp_seconds Last timestamp in seconds since epoch of the last dataset refresh
+# TYPE github_actions_workflow_last_refresh_timestamp_seconds gauge
+github_actions_workflow_last_refresh_timestamp_seconds 1.697328e+09
+				`,
 		},
 	} {
 		t.Run(testCase.metricName, func(t *testing.T) {
@@ -145,6 +169,12 @@ func TestCollector(t *testing.T) {
 					fetcher,
 					logger,
 					10*time.Minute,
+					actions.WithSinceFunc(
+						fixedSince(time.Second),
+					),
+					actions.WithNowFunc(
+						fixedNow(now),
+					),
 				)
 				registry = prometheus.NewRegistry()
 			)
@@ -165,3 +195,15 @@ func TestCollector(t *testing.T) {
 }
 
 func ptr[V any](v V) *V { return &v }
+
+func fixedNow(t time.Time) func() time.Time {
+	return func() time.Time {
+		return t
+	}
+}
+
+func fixedSince(d time.Duration) actions.SinceFunc {
+	return func(_, _ time.Time) time.Duration {
+		return d
+	}
+}
